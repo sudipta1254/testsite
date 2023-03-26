c47683bc899c0c905252959f66db8d8a68e7c920
sudipta1254 <115441575+sudipta1254@users.noreply.github.com>
2023-03-26 12:24:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function random() {
-    document.getElementById('get').style.backgroundImage = "url('https://source.unsplash.com/random/500x500?city,night')";
+    const w = 768;
+    const h = 1080;
+    //document.getElementById('get').style.backgroundImage = "url('https://source.unsplash.com/random/500x500?city,night')";
+    document.getElementById('get').style.backgroundImage ="url('https://source.unsplash.com/random/"+ w +"x"+ h +"?city,night')";
 }
 
 function reset() {
