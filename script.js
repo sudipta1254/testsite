6b258ffda70bad6cd9eeeaf2934dfeb01cf2abe0
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-06-02 10:03:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-function random() {
-    const w = 420; /*768*/
-    const h = 750; /*1080*/
-    //document.getElementById('get').style.backgroundImage = "url('https://source.unsplash.com/random/500x500?city,night')";
-    document.getElementById('get').style.backgroundImage ="url('https://source.unsplash.com/random/"+ w +"x"+ h +"?city,night')";
-    document.getElementById('pa').innerHTML = 'Generating!';
-}
+list = ['AdviceAnimals', 'MemeEconomy', 'ComedyCemetery', 'memes',
+        'dankmemes', 'PrequelMemes', 'terriblefacebookmemes',
+        'pewdiepiesubmissions', 'funny', 'teenagers', 'HistoryMemes',
+        'MoldyMemes', 'wholesomememes', 'raimimemes', 'okbuddyretard',
+        'comedyheaven', 'me_irl', 'crappyoffbrands', 'im14andthisisdeep',
+        'trippinthroughtime', '4chan', 'TheRealJoke', 'nobodyasked',
+        'wheredidthesodago', 'suicidebywords', 'madlads', 'puns',
+        'devilmaycry', 'PhotoshopBattles'];
 
-function reset() {
-    location.replace(document.baseURI);
-    //location.reload();
-    //window.location.reload(true);
-    //location.href = location.href;
-    //location.replace(location.href.split("#")[0]);
-    //window.location.href = window.location.href.split("#")[0];
-}
+
