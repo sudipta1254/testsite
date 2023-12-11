cec11f8dfde33c9f9195286e19499c73c08a37c5
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-11 22:19:59 +0530

    Update files

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+});
+
+app.get("/json", (req, res) => {
+  res.send({message: "Welcome to Express App"});
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
