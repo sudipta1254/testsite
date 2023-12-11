f8ab3780a74d2bb27bf4c4b4a4d67c8ecfee84ab
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-11 22:33:08 +0530

    Update script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Hello from Express</h1>");
 });
 
 app.get("/about", (req, res) => {
