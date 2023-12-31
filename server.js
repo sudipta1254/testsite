3ce9b40af943d45aaf720015323f5dc342c4ed0d
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-31 15:08:03 +0530

    Update js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 // Handle requests to the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // Start the server
