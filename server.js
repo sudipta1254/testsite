d3eabd28c06dd9648ea51ffb33fd67def72df2ea
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-31 15:32:50 +0530

    new1

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 9000;
+const port = 3000;
 
 // Serve static files (HTML, CSS, JS) from the 'public' directory
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // Handle requests to the root URL
 app.get('/', (req, res) => {
