0439b5be26170804937cac6b3be4cb86b37ca919
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-31 15:52:30 +0530

    new

---
+++ public/secrets.js
@@ -0,0 +1,6 @@
+const key = 'a1af1621-da48-4592-a132-52415d0cabd3'; /* For public */
+const key2 = '7e5231c8-8efc-402c-a160-6c769fe8e934';/* For dev */
+const PORT = 3000;
+
+export { key, key2, PORT };
+
