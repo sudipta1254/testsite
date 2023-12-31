d98f312c3d194c6fe226f7ca33cb77bcdaa8b7ae
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-31 15:23:49 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 9000;
+
+// Serve static files (HTML, CSS, JS) from the 'public' directory
+// app.use(express.static('public'));
+
+// Handle requests to the root URL
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
