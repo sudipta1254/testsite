5c88baf5919d3798cf8a5821ff671b3bbbd4fda3
Sudipta Singha <singhaashoka03@gmail.com>
2023-12-31 15:39:52 +0530

    new

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 // Serve static files (HTML, CSS, JS) from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 
 // Handle requests to the root URL
 app.get('/', (req, res) => {
