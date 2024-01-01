3c26993a93286619e44dec08f20753b8b1878f30
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-01-01 20:55:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import { path } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 //const __dirname = dirname(fileURLToPath(import.meta.url));
 
