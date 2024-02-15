c26aa50e8cab74bb78247e73752f72776cf059c3
Sudipta Singha <singhaashoka03@gmail.com>
2024-02-15 07:12:17 +0530

    Bug fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "docs")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/index.html");
